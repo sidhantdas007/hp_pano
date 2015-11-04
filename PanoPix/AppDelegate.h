cda01455635f11454fdb0c9bb2a111d60d3d82b1
James <trask@hp.com>
2015-11-04 10:00:53 -0800

    Initial Commit

---
+++ PanoPix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PanoPix
+//
+//  Created by James Trask on 11/4/15.
+//  Copyright © 2015 HP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PanoPix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PanoPix
+//
+//  Created by James Trask on 11/4/15.
+//  Copyright © 2015 HP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
