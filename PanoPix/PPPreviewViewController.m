ac4534a1f8947f809a8795f737954b2b49213c70
James <trask@hp.com>
2015-11-09 10:21:20 -0800

    rotation handling

--- PanoPix/PPPreviewViewController.m
+++ PanoPix/PPPreviewViewController.m
@@ -36,6 +36,35 @@
     [[UIDevice currentDevice] setValue:value forKey:@"orientation"];
 }
 
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+{
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    // Code here will execute before the rotation begins.
+    // Equivalent to placing it in the deprecated method -[willRotateToInterfaceOrientation:duration:]
+    
+//    self.paperView.hidden = YES;
+    
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        
+        // Place code here to perform animations during the rotation.
+        // You can pass nil or leave this block empty if not necessary.
+
+        [self layoutScrollViews];
+        [self createImageViews];
+        
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+
+        // Code here will execute after the rotation has finished.
+        // Equivalent to placing it in the deprecated method -[didRotateFromInterfaceOrientation:]
+        
+        [self layoutScrollViews];
+        [self createImageViews];
+//        self.paperView.hidden = NO;
+
+    }];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
